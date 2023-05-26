176327c9e7e92959a6a1da981e96d8ed5dcddf2b
JoaoOtavioS <joaootaviostivi2@gmail.com>
2023-05-26 18:24:13 -0300

    update import fs to node:fs

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 /*
-Copyright 2021 Square Cloud - All rights reserved.
+Copyright 2021-2023 Square Cloud - All rights reserved.
 */
 
-const fs = require('fs');
+const { existsSync, readFileSync } = require("node:fs");
 const format = (value) => Math.round(value / Math.pow(1024, 2), 2);
 
 // Thanks arthurcosta for the refactored code.
 function getMemDetail(cat) {
     const path = `/sys/fs/cgroup/memory/memory.${cat}_in_bytes`;
 
-    if (fs.existsSync(path))
-        return format(fs.readFileSync(path)).toString();
+    if (existsSync(path))
+        return format(readFileSync(path)).toString();
     else
-        throw new Error('The requested data could not be found.');
+        throw new Error("The requested data could not be found.");
 }
 
 module.exports = {
-    ramUsed: () => getMemDetail('usage'),
-    ramTotal: () => getMemDetail('limit'),
+    ramUsed: () => getMemDetail("usage"),
+    ramTotal: () => getMemDetail("limit"),
     ram: () => `${this.ramUsed()}/${this.ramTotal()}MB`
 }; 
